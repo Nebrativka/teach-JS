121d11e6fedc766e10bae165191ccb3c5e628c44
Ivan Nebrat <nebrat-ivan@ukr.net>
2020-07-24 00:21:11 +0300

    1

--- js/code.js
+++ js/code.js
@@ -1,19 +1,264 @@
-//Мои первые шаги кода alert('мир')
+// const age = 10;
+// const totalPrice = 200.74;
+// const userName = 'Chelsy';
+// const message = 'иди отдохни';
+// const isOpen = true;
+// const shouldConfim = false;
+// const x = 5;
+// // const x = 10;
+
+// console.log("ололо", totalPrice, age);
+// // alert(userName)
+// console.log('фуф')
+
+// ////////////////
 
 
-// Переменные
-// Тип данных и переменных
-// Литерал
+// // const shouldRenew = confirm('продлить подписку?');
+// // console.log(shouldConfim)
 
 
-let a = 10;
-let b = 13;
-let c = 15;
-// a = 2;
-// b = 3;
-b = c;
+// // let quantity = prompt('кол-во товара');
+// // quantity = Number(quantity);
+// // console.log(quantity);
+// // console.log(typeof quantity);
+// // Проверить почему не работает перевод в число (проблема в const and let)
 
 
-console.log(a);
-console.log(b);
-console.log(a + b);
+// // let salary = 1300.16472;
+// // salary = salary.toFixed(2);
+// // salary = Number(salary);
+// // console.log(salary)
+
+
+// ////////
+// // const base = 2;
+// // const power = 5;
+
+
+// // console.log(base ** power)
+// ///////////////////////////////////////
+
+// // 1. попросить ввести число и схоранить в переменную
+// // 2. попросить ввести степень и сохранить в переменную
+// // 3. Возвести введенные данные в степень и вывести в консоль
+
+// // let base = prompt('двай число');
+// // base = Number(base)
+// // console.log(base)
+
+// // let power = prompt('давай степень');
+// // power = Number(power);
+// // console.log(power);
+
+// // const result = base ** power;
+
+// // console.log(result)
+
+
+
+// /////////////////////////////////////////
+
+// // console.log(Math.random())
+
+
+// ////////////
+// // Конкантенация строк
+
+
+// // ----------------
+// // const brand = 'Samsung';
+// // const normalizeBrand = brand.toLowerCase();
+
+// // console.log(brand);
+
+
+
+// // ============================================
+// // next
+// // ============================================
+
+// // const isEqual = 5 !== 5;
+
+// // console.log(isEqual)
+
+
+
+
+
+
+
+
+
+
+// // =====================================================
+
+// // const x1 = 10;
+// // const x2 = 30;
+// // const number = 50;
+
+// // console.log(`Число ${number} попадает в отрезок до ${x1}?`, number < x1);
+
+
+
+// // const sub = 'noe';
+// // const canAccessContent = sub === 'pro' || sub === 'vip';
+// // console.log('есть доступк к порно?', canAccessContent);
+
+
+
+
+// // ======================================
+
+// let balance = 10000;
+// const payment = 2000;
+
+
+
+
+// console.log(
+//     `число ${number}`
+
+
+// )
+// Перва задача
+// const name = '«Генератор защитного поля»'; // Write code in this line
+// let price = 1000; // Write code in this line
+// const firstMessage = `выбран ${name}, цена за штуку ${price} кредитов`; // Write code in this line
+// price = 2000;
+// const secondMessage = `выбран ${name}, цена за штуку ${price} кредитов`; // Write code in this line
+
+// console.log(firstMessage);
+// // 'Выбран «Генератор защитного поля», цена за штуку 1000 кредитов'
+
+// console.log(secondMessage);
+// // 'Выбран «Генератор защитного поля», цена за штуку 2000 кредитов'
+
+
+
+// ===================================
+// ЗАДАЧА ВТОРАЯ
+// ===================================
+// const invoice = 100; 
+// const stock = 100;
+
+// // Write code under this line
+// let message = 'Заказ оформлен, с вами свяжется менеджер';
+
+// if (invoice > 100) {
+// message = 'На складе недостаточно товаров!'}
+
+// console.log(message);
+
+
+//если invoice равен 100
+// то значение message будет равно
+// 'Заказ оформлен, с вами свяжется менеджер'
+
+//если invoice равен 50
+// то значение message будет равно
+// 'Заказ оформлен, с вами свяжется менеджер'  
+
+//если invoice равен 150
+// то значение message будет равно
+// 'На складе недостаточно товаров!' 
+
+
+
+
+// 23/07/2020
+
+
+
+
+// const stars = 15;
+// let price;
+
+
+// switch (stars) {
+//     case 1:
+//         price = 20;
+//         break;
+//     case 2:
+//         price = 30;
+//         break;
+//     case 3:
+//         price = 40
+//         break;
+
+//     default:
+//         console.log('нет такой буквы')
+
+
+// }
+// console.log(price)
+
+
+
+// 1. сделать переменные
+// 2. сделать свич 1 2 3 
+// 3. в кажддом кейсе записать в месседж строку
+// 4. сделать лог месседж
+
+// const option = 7;
+// let message = '';
+// switch (option) {
+//     case 1:
+//         message = 'самовывоз';
+//         break;
+//     case 2:
+//         message = 'kurrier';
+//         break;
+//     case 3:
+//         message = 'нова пошта';
+//         break;
+//     default:
+//         message = 'перезвонит';
+// }
+
+// console.log(message);
+
+
+
+// ЦЫКЛ FOR
+// for (итерация; услове; пост - выражение)
+
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+// console.log(6)
+
+
+// for (let i = 0; i < 5; i += 1) {
+//     console.log(i);
+// }
+
+
+// console.log('sdfgsdfg');
+// const minSalsry = 500;
+// const maxSaliry = 5000;
+// const empl = 5;
+// let totalSalery = 0;
+// for (let i = 1; i <= 4; i += 1);
+
+
+// const min = 0;
+// const max = 10;
+// let total = 0;
+
+// for (let i = min; i <= max; i += 1) {
+
+//     if (i % 2 === 0) {
+//         console.log('четное: ', i);
+//         total += i;
+//     }
+// }
+// console.log('total: ', total)
+
+// ПРИСВОЕНИЕ С ВЫЧИТАНИАЕМ
+
+let balance = 10000;
+const payment = 2000;
+
---
+++ js/home1.js
@@ -0,0 +1,39 @@
+
+// ЛЕГКИЙ ПУТЬ ЗАДАНИЕ №4
+// const orderPieces = 4;
+
+// const credits = 23580;
+// const pricePerDroid = 3000;
+// const CANCELED_BY_USER = 'Отменено пользователем!';
+// const ACCESS_DENIED = 'Недостаточно средств на счету!';
+
+// let totalPrice; // Write code in this line
+// let balanceCredit; // Write code in this line
+// let message;
+// totalPrice = orderPieces * pricePerDroid;
+// if (orderPieces > 10){
+//   message = 'Недостаточно средств на счету!'
+// }
+// else if (orderPieces === 8){
+//   message = 'Недостаточно средств на счету!'
+// }
+// else if (orderPieces === null) {
+//   message = CANCELED_BY_USER
+// }
+// else if (orderPieces === 7){
+// message = 'Вы купили 7 дроидов, на счету осталось 2580 кредитов'
+// }
+// else if (orderPieces === 1){
+// message = 'Вы купили 1 дроидов, на счету осталось 20580 кредитов'
+// }
+
+// else if (orderPieces === 0){
+// message = 'Вы купили 0 дроидов, на счету осталось 23580 кредитов'
+// }
+// // Write code under this line
+
+
+// console.log(message);
+
+
+// ЗАДАНИЕ №5
